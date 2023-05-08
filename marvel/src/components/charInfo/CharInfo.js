ac6a0199605a153c8aa614e53db95810497893fd
Maryia Engel <maryiaengel@gmail.com>
2023-05-08 12:50:52 -0700

    marvel

---
+++ marvel/src/components/charInfo/CharInfo.js
@@ -0,0 +1,107 @@
+import { useState, useEffect } from "react";
+
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Skeleton from "../skeleton/Skeleton";
+
+import "./charInfo.scss";
+
+const CharInfo = (props) => {
+  const [char,setChar] =useState(null);
+  const [loading,setLoading] =useState(false);
+  const [error,setError] =useState(false);
+
+  const marvelService = new MarvelService();
+
+  useEffect(() => {
+    updateChar();
+  }, [props.charId]);
+
+ const updateChar = () => {
+    const { charId } = props;
+    if (!charId) {
+      return;
+    }
+    onCharLoading();
+    marvelService
+      .getCharacter(charId)
+      .then(onCharLoaded)
+      .catch(onError);
+  };
+
+  const onCharLoaded = (char) => {
+    setLoading(false);
+    setChar(char);
+  };
+
+  const onCharLoading = () => {
+    setLoading(true);
+  };
+
+  const onError = () => {
+    setLoading(false);
+    setError(true);
+  };
+
+    const skeleton = char || loading || error ? null : <Skeleton />;
+    const errorMesage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+    return (
+      <div className="char__info">
+        {skeleton}
+        {errorMesage}
+        {spinner}
+        {content}
+      </div>
+    );
+  }
+
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki, comics } = char;
+
+  let imgStyle = { objectFit: "cover" };
+  if (
+    char.thumbnail.path ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle.objectFit = "contain";
+  }
+
+  return (
+    <>
+      <div className="char__basics">
+        <img src={thumbnail} alt="abyss" style={imgStyle} />
+        <div className="char__info-name">{name}</div>
+        <div className="char__btns">
+          <a href={homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+
+      <div className="char__descr">{description}</div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length > 0 ? null : "There is no comics with this character"}
+        {comics.map((item, i) => {
+          console.log(item, i);
+          //eslint-disable-next-line array-callback-return
+          if (i > 9) return;
+          return (
+            <li key={i} className="char__comics-item">
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default CharInfo;
